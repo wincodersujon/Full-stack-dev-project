789a130d4c28657d819d5ba04b2c3272f5e50fb2
sujon biswas <sujonbiswas2391@gmail.com>
2023-08-29 01:57:32 +0600

    RL

---
+++ src/components/admin/Dashboard.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Dashboard(){
+    return (
+        <h1>I am Dashboard</h1>
+    )
+}
+export default Dashboard;
---
+++ src/layouts/admin/MasterLayout.js
@@ -0,0 +1,49 @@
+import React from "react";
+import '../../assets/admin/css/styles.css';
+import '../../assets/admin/js/scripts.js';
+
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+import Footer from './Footer';
+import routes from '../../routes/routes.js';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+
+const MasterLayout = () => {
+  return (
+    <div className="sb-nav-fixed">
+      <Navbar />
+      <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+          <Sidebar />
+        </div>
+        <div id="layoutSidenav_content">
+          <main>
+            <Switch>
+              {routes.map((route, index) => {
+                return (
+                  route.component && (
+                    <Route
+                      key={index}
+                      path={route.path}
+                      exact={route.exact}
+                      name={route.name}
+                      render={(props) => (
+                        <route.component {...props} />
+                      )}
+                    />
+
+                  )
+                )
+              })}
+              <Redirect from="admin" to="/admin/dashboard" />
+            </Switch>
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MasterLayout;
